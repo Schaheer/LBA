87bfd4155aa736e017cb89419b7d1de27143cfba
Muhammad Irfan Tarar <85341777+Irfantarar@users.noreply.github.com>
2022-12-10 16:29:32 +0500

    - Bug Fixes 1

--- ablCordova/platforms/ios/ABL/Plugins/ABLPlugin/Controllers/Open Account/Confirmation/ConfirmationVC.swift
+++ ablCordova/platforms/ios/ABL/Plugins/ABLPlugin/Controllers/Open Account/Confirmation/ConfirmationVC.swift
@@ -10,7 +10,10 @@ import UIKit
 final class ConfirmationVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupGestureRecognizers()
     }
+    @IBOutlet weak var appEmail: LabelSetting!
+    @IBOutlet weak var abmNumber: LabelSetting!
     
     @IBAction func noThanksTapped(_ sender: UIButton){
         
@@ -21,5 +24,59 @@ final class ConfirmationVC: UIViewController {
     @IBAction func feedbackBtnTapped(_ sender: UIButton){
         
         //
+    }
+    
+    
+    //Irfan
+    
+    private func setupGestureRecognizers() {
+        
+        let helpLineNumbertap = UITapGestureRecognizer(target: self, action: #selector(openNumberInNumPad))
+        
+        abmNumber.isUserInteractionEnabled = true
+        
+        abmNumber.addGestureRecognizer(helpLineNumbertap)
+        
+        
+        
+        let emailtap = UITapGestureRecognizer(target: self, action: #selector(openEmail))
+        
+        appEmail.isUserInteractionEnabled = true
+        
+        appEmail.addGestureRecognizer(emailtap)
+        
+    }
+    
+    //Irfan
+    
+    @objc private func openNumberInNumPad() {
+        let phoneNumber: String = "telprompt://111225225"
+        
+        if let appURL = URL(string: phoneNumber){
+            
+            
+            
+            if #available(iOS 10.0, *) {
+                
+                UIApplication.shared.open(appURL, options: [:], completionHandler: nil)
+                
+            } else {
+                
+                UIApplication.shared.openURL(appURL)
+                
+            }
+            
+        }
+    }
+    @objc private func openEmail() {
+        
+        if let appURL = URL(string: "mailto:\(appEmail.text!)"){
+            
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(appURL, options: [:], completionHandler: nil)
+            } else {
+                UIApplication.shared.openURL(appURL)
+            }
+        }
     }
 }
