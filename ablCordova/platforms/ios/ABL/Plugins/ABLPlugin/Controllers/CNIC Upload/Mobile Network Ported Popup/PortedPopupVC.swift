91b9ac3422cac16a4583490fed44ce22c31bc2cb
ahmedshakeelpk <ahmedshakeelpk@gmail.com>
2022-12-13 18:58:37 +0500

    debit cared i button functiono

--- ablCordova/platforms/ios/ABL/Plugins/ABLPlugin/Controllers/CNIC Upload/Mobile Network Ported Popup/PortedPopupVC.swift
+++ ablCordova/platforms/ios/ABL/Plugins/ABLPlugin/Controllers/CNIC Upload/Mobile Network Ported Popup/PortedPopupVC.swift
@@ -10,14 +10,31 @@ import UIKit
 final class PortedPopupVC: UIViewController {
     
     var portedMobileNetwork: (() -> Void)?
+    var message: String?
+    var buttonTitle: String?
+    @IBOutlet weak var doneButton: ButtonSetting!
+    @IBOutlet weak var labelMessage: LabelSetting!
     
+   
+    @IBOutlet weak var cancelButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if message != nil {
+            labelMessage.text = message
+        }
+        if buttonTitle != nil {
+            doneButton.setTitle(buttonTitle, for: .normal)
+        }
         view.backgroundColor = UIColor.black.withAlphaComponent(0.7)
     }
     
     @IBAction func doneTapped(_ sender: UIButton) {
+        dismiss(animated: true) {
+            self.portedMobileNetwork?()
+        }
+    }
+    @IBAction func cancelButton(_ sender: Any) {
         dismiss(animated: true) {
             self.portedMobileNetwork?()
         }
